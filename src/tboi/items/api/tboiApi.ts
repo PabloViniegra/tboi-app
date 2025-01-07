df22b369224e94283257c67f172216d728cbb9a4
PabloViniegra <pablovpmadrid@gmail.com>
2025-01-07 23:16:52 +0100

    add auth api

--- src/tboi/items/api/tboiApi.ts
+++ src/tboi/items/api/tboiApi.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_APP_API_AUTHORIZATION;
 
 const tboiApi = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    "X-API-Key": API_KEY,
+  }
 });
 
 export default tboiApi;
